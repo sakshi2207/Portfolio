e0471caa9b7c2a58c5dfbb23aa430bdf671f503e
sakshi <40893075+sakshi2207@users.noreply.github.com>
2020-12-30 19:25:43 +0530

    Add cursor

--- script.js
+++ script.js
@@ -1,4 +1,60 @@
 AOS.init();
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+
+var elements = document.getElementsByClassName('txt-rotate');
+for (var i=0; i<elements.length; i++) {
+var toRotate = elements[i].getAttribute('data-rotate');
+var period = elements[i].getAttribute('data-period');
+if (toRotate) {
+  new TxtType(elements[i], JSON.parse(toRotate), period);
+
+}
+}   
+
+};
+
+
 
 var menu = document.querySelector('.menu');
 var bg = document.querySelector('.bg');
@@ -18,15 +74,17 @@ window.addEventListener('scroll', function () {
     scroll.classList.toggle("active", window.scrollY > 500)
 })
 
+
 function scrollToTop() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     })
 }
+
 function sendEmail() {
     Email.send({
-        SecureToken: "xxx",
+        SecureToken: "b65dec8c-52df-43b3-afb3-77058ec797ae",
         To: 'maliksakshi248@gmail.com',
         From: document.getElementsByClassName("email")[0].textContent,
         Subject: "Website email",
